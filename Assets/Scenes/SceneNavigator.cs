d04b2e322d3852f29889db3594c4ca3a15c6a656
lmlynik <lukasz.mlynik@hotmail.com>
2016-01-28 10:49:29 +0100

    Fix missing scenes

---
+++ Assets/Scenes/SceneNavigator.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneNavigator : MonoBehaviour
+{
+
+    public Object cardBattleScene;
+
+    public void NavigateToCardBattle()
+    {
+        SceneManager.LoadScene(cardBattleScene.name);
+    }
+}
