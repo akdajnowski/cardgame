4e9af8943cf7ac15368416c09ee72f5600137446
rafal-dziegielewski <rafal.dziegielewski@gmail.com>
2016-01-29 21:45:07 +0100

    EndCredits

---
+++ Assets/Scenes/MainMenu/TextScrolling.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TextScrolling : MonoBehaviour {
+    private Text credits;
+    public int ScrollSpeed;
+
+	void Start () {
+        credits = gameObject.GetComponent<Text>();
+	}
+	
+	void Update () {
+        credits.transform.Translate(new Vector3(0, ScrollSpeed, 0));
+	}
+}
