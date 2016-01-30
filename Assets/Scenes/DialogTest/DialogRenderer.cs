529a9adc3dec537178261b418ce2e69a52070aa8
Łukasz Młynik <lukasz.mlynik@hotmail.com>
2016-01-31 00:44:32 +0100

    Made world event script for generic places.

--- Assets/Scenes/DialogTest/DialogRenderer.cs
+++ Assets/Scenes/DialogTest/DialogRenderer.cs
@@ -32,7 +32,9 @@ public class DialogRenderer : MonoBehaviour
         string currentIsland = Store.OverworldState.CurrentIsland;
 
 
-        var dialog = dialogs.Dialogs.First(x => x.Id == currentIsland);
+        var dialog = dialogs.Dialogs.FirstOrDefault(x => x.Id == currentIsland);
+        if (dialog == null)
+            throw new DialogMissingException(currentIsland);
         Render(dialog);
     }
 
@@ -41,5 +43,13 @@ public class DialogRenderer : MonoBehaviour
         var textPanelBehaviour = DialogPanel.GetComponent<TextPanelBehaviour>();
         textPanelBehaviour.SelectionCallback(() => Store.AdvanceState(Scenes.CardBattle));
         textPanelBehaviour.SetDialog(dialog);
+    }
+
+    public class DialogMissingException : Exception
+    {
+        public DialogMissingException(string dialogKey) : base("Missing dialog for " + dialogKey)
+        {
+
+        }
     }
 }
