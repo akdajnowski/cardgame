5144cc66d579a7d1287072486ddf97f6c244af78
Łukasz Młynik <lukasz.mlynik@hotmail.com>
2016-01-30 23:18:35 +0100

    Added cursor

---
+++ Assets/Scenes/CursorApplier.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CursorApplier : MonoBehaviour
+{
+    public Texture2D cursor;
+
+    // Use this for initialization
+    void Start()
+    {
+        cursor = Resources.Load<Texture2D>(@"cursor");
+        Cursor.SetCursor(cursor, Vector2.zero, CursorMode.Auto);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
