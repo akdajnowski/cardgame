44c768a2369132444e329d9f5aa2a21c7a1ae5cc
rafal-dziegielewski <rafal.dziegielewski@gmail.com>
2016-01-31 14:04:32 +0100

    Added resources for Belt, Rock and Necklace

--- Assets/Scenes/CardBattle/CardAssetsProvider.cs
+++ Assets/Scenes/CardBattle/CardAssetsProvider.cs
@@ -9,7 +9,7 @@ public class CardAssetsProvider
     public CardAssetsProvider ()
     {
         Icons = Resources.LoadAll ("icons").OfType<Sprite> ().ToList ();
-        Icons.AddRange (LoadSprites ("mjornir", "arrow", "arrows", "sword_diag", "two_swords_diag", "mail", "shield", "spear", "ring"));
+        Icons.AddRange (LoadSprites ("mjornir", "arrow", "arrows", "sword_diag", "two_swords_diag", "mail", "shield", "spear", "ring", "axe", "belt", "rock", "necklace"));
         Debug.Log (Icons.Count.ToString ());
     }
 
